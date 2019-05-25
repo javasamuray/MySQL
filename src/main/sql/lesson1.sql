a9ca80a0f158c1298f01ef4649345c494424f86a
Pavel Matvienko <javasamuray@mail.ru>
2019-05-25 10:01:37 +0700

    1

--- src/main/sql/lesson1.sql
+++ src/main/sql/lesson1.sql
@@ -1,10 +1,7 @@
-/*
-TODO: * Создаем базу данных для обучения - СУБД PostgreSQL -> sudo -u coder psql
-*/
+sudo -u coder psql
 
 CREATE DATABASE mysql_db;
-
 CREATE USER Admin_db WITH password 'admin';
-
+GRANT ALL ON DATABASE mysql_db TO Admin_db;
+ALTER DATABASE mysql_db OWNER TO admin_db;
 ALTER ROLE admin_db SUPERUSER NOCREATEDB CREATEROLE INHERIT LOGIN;
-
